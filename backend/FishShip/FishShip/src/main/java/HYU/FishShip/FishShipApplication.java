41df9ce3d86ad9980594a91087aa4e7ad556c208
LeeHyunWoo02 <hyoun0824@naver.com>
2025-05-05 23:31:15 +0900

    docs : 백앤드 개발 환경 구축

---
+++ backend/FishShip/FishShip/src/main/java/HYU/FishShip/FishShipApplication.java
@@ -0,0 +1,13 @@
+package HYU.FishShip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FishShipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FishShipApplication.class, args);
+	}
+
+}
---
+++ backend/FishShip/FishShip/src/test/java/HYU/FishShip/FishShipApplicationTests.java
@@ -0,0 +1,13 @@
+package HYU.FishShip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FishShipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
