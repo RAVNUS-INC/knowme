63e2aede2462b68c3c4c7846df2860e015bc6a12
jangjimin <jangjimin9766@gmail.com>
2025-05-23 21:08:29 +0900

    fix: remove unused import and clean dev_dependencies

--- lib/features/home/controllers/home_controller.dart
+++ lib/features/home/controllers/home_controller.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import '../../ai_analysis/views/ai_analysis_screen.dart';
 import '../../../routes/routes.dart';
 
 class HomeController extends GetxController {
